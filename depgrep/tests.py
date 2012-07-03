319e286b6112ccb02b4f221375242a088bc46e16
Jonathan Lange <jml@canonical.com>
2012-07-03 10:59:46 +0100

    TODOs

--- depgrep/tests.py
+++ depgrep/tests.py
@@ -1,3 +1,4 @@
+
 from testtools import TestCase
 from testtools.matchers import SameMembers
 
@@ -37,3 +38,24 @@ class TestImports(TestCase):
     def test_multi_from_import(self):
         imports = find_imports("from foo import bar, baz")
         self.assertThat(imports, SameMembers(['foo.bar', 'foo.baz']))
+
+
+# TODO: Actually use a Python syntax parser.
+
+# TODO: Support multi-line 'from foo import (...)' style imports
+
+# TODO: For found imports, distinguish which part is the module and which part
+# is the name within the module. (use sys.path)
+
+# TODO: Command-line script
+
+# TODO: Show files for found modules (use sys.path)
+
+# TODO: Recursively search many files and generate a set of all things that
+# they import that are outside that set of files.  (Preserve which file
+# imports what).
+
+# TODO: Search the sys.path to find things that import X.
+
+# XXX: Not sure what to do about imports in function definitions, for example.
+
