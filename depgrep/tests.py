d10827c568680995ed63c9f3f50107a22e0da41f
Jonathan Lange <jml@canonical.com>
2012-07-03 10:50:34 +0100

    Really crummy implementation, but some tests that will help us.

--- depgrep/tests.py
+++ depgrep/tests.py
@@ -1,13 +1,39 @@
 from testtools import TestCase
-from testtools.matchers import Equals
+from testtools.matchers import SameMembers
 
 
 def find_imports(python_code):
-    return []
+    """Find all of the imports in 'python_code'."""
+    imports = []
+    for line in python_code.splitlines():
+        if line.startswith('import '):
+            modules = [m.strip() for m in line[7:].split(',')]
+            imports.extend(modules)
+        if line.startswith('from '):
+            start, end = line[5:].split(' import ')
+            imports.extend(
+                ['%s.%s' % (start, m.strip()) for m in end.split(',')])
+    return imports
 
 
 class TestImports(TestCase):
 
     def test_empty(self):
         imports = find_imports("")
-        self.assertThat(imports, Equals([]))
+        self.assertThat(imports, SameMembers([]))
+
+    def test_import(self):
+        imports = find_imports("import foo\n")
+        self.assertThat(imports, SameMembers(['foo']))
+
+    def test_multi_import(self):
+        imports = find_imports("import foo, bar\n")
+        self.assertThat(imports, SameMembers(['foo', 'bar']))
+
+    def test_from_import(self):
+        imports = find_imports("from foo import bar")
+        self.assertThat(imports, SameMembers(['foo.bar']))
+
+    def test_multi_from_import(self):
+        imports = find_imports("from foo import bar, baz")
+        self.assertThat(imports, SameMembers(['foo.bar', 'foo.baz']))
